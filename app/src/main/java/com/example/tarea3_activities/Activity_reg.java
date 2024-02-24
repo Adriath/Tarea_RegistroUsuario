49cd432f6da43e69cf955fa708e97cd1f3fe2597
Adriath <adrianarjonabravo@gmail.com>
2024-02-24 12:47:15 +0100

    Creada nueva pantalla (Activity). No está completa, le faltan migas de pan y botón de retroceder

---
+++ app/src/main/java/com/example/tarea3_activities/Activity_reg.java
@@ -0,0 +1,14 @@
+package com.example.tarea3_activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg);
+    }
+}
